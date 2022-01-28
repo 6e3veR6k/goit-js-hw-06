af93165f31604ba012a70ea90f0476d839b05be0
6e3veR6k <bezyn4uk@gmail.com>
2022-01-28 22:58:41 +0200

    init empty project

---
+++ js/task-01.js
@@ -0,0 +1,18 @@
+// Посчитать и вывести в консоль количество
+// категорий в ul#categories, то есть элементов li.item.
+// Для каждого элемента li.item в списке ul#categories,
+// найти и вывести в консоль текст заголовка элемента (тега <h2>)
+// и количество элементов в категории (всех вложенных в него <li>).
+
+/* Результат:
+    Number of categories: 3
+
+    Category: Animals
+    Elements: 4
+
+    Category: Products
+    Elements: 3
+
+    Category: Technologies
+    Elements: 5
+*/
