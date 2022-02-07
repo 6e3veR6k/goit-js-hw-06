a0617126e0ce7d0d4b66e869c1c4f2d146a30d36
bezyn4uk <bezyn4uk@gmail.com>
2022-02-07 22:16:35 +0200

    task 1 finished

--- js/task-01.js
+++ js/task-01.js
@@ -16,3 +16,42 @@
     Category: Technologies
     Elements: 5
 */
+
+class CategoryList {
+  #listRef;
+
+  constructor(id) {
+    this.#listRef = document.getElementById(id);
+  }
+
+  get length() {
+    // 1 variant
+    return this.#listRef.childElementCount;
+
+    // 2 variant
+    // return this.#listRef.querySelectorAll('.item').length;
+  }
+
+  get categoriesInfo() {
+    const listOfRefsCategories = this.#listRef.children;
+    return [...listOfRefsCategories].map((el) => {
+      return {
+        categoryName: el.firstElementChild.textContent,
+        categoryListItems: [...el.querySelector('ul').children].map((item) => item.textContent),
+        categoryListLength: el.querySelector('ul').childElementCount,
+      };
+    });
+  }
+
+  toString() {
+    let result = `Number of categories: ${this.length}\n\n`;
+    this.categoriesInfo.forEach(element => {
+        result += `Category: ${element.categoryName}\nElements:${element.categoryListLength}\n\n`;
+    });
+    return result;
+  }
+}
+
+const k = new CategoryList('categories');
+console.log(k.toString());
+
